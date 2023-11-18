5142d3047c92621602c42d669409a39538c268fb
gguilhermelopes <guilhermelopes275@gmail.com>
2023-11-18 15:37:06 -0300

    feat: remove dist

--- dist/routes/Routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const AuthController_1 = require("../controllers/AuthController");
-const router = (0, express_1.Router)();
-router.post('/login', AuthController_1.login);
-exports.default = router;
