f784e47af168616fa9f14ff5ca0ef2d3c2fd4c09
Guilherme <guisou1902@gmail.com>
2023-11-22 12:23:55 -0300

    fix: added app.use router to initialize the routes file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express"
-
+import router from "./routes/Routes"
 const app = express()
 
 app.use(express.json())
+
+app.use(router)
 
 app.listen("3000", () => {
   console.log("Server is running on port 3000")
