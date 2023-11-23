71e12c1fbc3cf45d1a4b74ebe8a61ae4e09a15cf
Caio Alves <caioalvesfig@gmail.com>
2023-11-23 12:31:55 -0300

    password length validation

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { cadastro } from "../controllers/SignInController";
 import { login } from "../controllers/AuthController";
-import { forgotPassword, resetPassword, changePassword } from "../controllers/ForgotPasswordController";
+import { forgotPassword } from "../controllers/ForgotPasswordController";
+import { resetPassword } from "../controllers/ForgotPasswordController";
+import { changePassword } from "../controllers/ForgotPasswordController";
 
 const router = Router();
 
